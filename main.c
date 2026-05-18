2ff3c7edf0db009817ce2c6f6494b822cf6ef96b
gjotuur <v.chekhovskyi@proton.me>
2026-05-18 12:59:48 +0300

    Initial commit helloworld_btw

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <openssl/rand.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdarg.h>
+
+typedef struct{
+    uint64_t size;
+    double* samples;
+}stat_sample_t;
+
+int main(){
+
+    return 0;
+}
+
+
+//NULL
